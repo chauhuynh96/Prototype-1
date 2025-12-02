884c6c138f902c6606e4909c1bee7160593e4475
chauhuynh96 <huynhminhchau43dn@gmail.com>
2025-12-02 16:57:48 +0700

    ....

---
+++ Prototype 1/Assets/Scripts/FollowPlayer1.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset1 = new Vector3(0f, 2f, 1f);
+    private Vector3 offset2 = new Vector3(0f, 3.62f, -5.16f);
+    public Camera secondCamera;
+    public Camera currentCamera;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentCamera.enabled = true;
+        currentCamera.GetComponent<AudioListener>().enabled = true;
+        secondCamera.enabled = false;
+        secondCamera.GetComponent<AudioListener>().enabled = false;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        
+        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("Đã nhấn phím C");
+            currentCamera.enabled = false;
+            currentCamera.GetComponent<AudioListener>().enabled = false;
+            secondCamera.enabled = true;
+            secondCamera.GetComponent<AudioListener>().enabled = true;
+
+        }
+        else if(Input.GetKeyDown(KeyCode.Z))
+        {
+            currentCamera.enabled = true;
+            currentCamera.GetComponent<AudioListener>().enabled = true;
+            secondCamera.enabled = false;
+            secondCamera.GetComponent<AudioListener>().enabled = false;
+        }
+
+        switch (currentCamera.enabled)
+        {
+            case true:
+                currentCamera.transform.position = player.transform.position + offset1;
+                break;
+            case false:
+                secondCamera.transform.position = player.transform.position + offset2;
+                break;
+        }
+      
+    }
+}
---
+++ Prototype 1/Assets/Scripts/PlayerController1.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        horizontalInput = Input.GetAxis("Horizontal1");
+        forwardInput = Input.GetAxis("Vertical1");
+
+        // run forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
